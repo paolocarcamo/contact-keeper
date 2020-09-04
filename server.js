2ab291a5570bb5b210966fa8a660cfcb7e6b95a2
Paolo Carcamo <Pogi@Paolo-Carcamo.local>
2020-09-04 15:42:47 -0700

    Express server setup with initial routes

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get("/", (req, res) => {
   });
 });
 
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
